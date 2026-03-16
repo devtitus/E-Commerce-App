53fdbc43d130f0eb8dda7fe62b6569bf1c733e2e
devtitus <m.works.gd@gmail.com>
2026-03-16 13:09:13 +0530

    refactor: optimize cart page layout and typography for better responsiveness
    - Adjusted cart page container to handle overflow properly on different screen sizes
    - Updated font sizes for better readability across breakpoints
    - Modified cart item card layout to improve alignment and spacing
    - Enhanced responsive design for cart items and summary section

--- app/components/CartItemCard.tsx
+++ app/components/CartItemCard.tsx
@@ -19,17 +19,17 @@ const CartItemCard = ({ item }: { item: CartItem }) => {
     const dispatch = useDispatch();
 
     return (
-        <div className='bg-white shadow-xs rounded-lg border border-black/10 p-5 h-47.5'>
-            <div className='flex flex-row items-start gap-3 h-full'>
+        <div className='bg-white shadow-xs rounded-lg border border-black/10 p-5 h-fit md:h-47.5'>
+            <div className='flex flex-col md:flex-row items-center md:items-start gap-3 h-full'>
                 <Image width={150} height={150} src={item.thumbnail} alt={item.title} className='rounded-lg object-contain w-37.5 h-37.5' />
                 <div className='flex flex-col justify-between w-full h-full'>
                     <div className='w-full flex flex-col'>
                         <div className='flex flex-row justify-between mb-1 w-full'>
-                            <span className='text-xl text-black/90 font-medium'>{item.title}</span>
-                            <span className='text-xl text-black/90 font-bold'>${item.price * item.quantity}</span>
+                            <span className='text-lg md:text-xl text-black/90 font-medium'>{item.title}</span>
+                            <span className='text-lg md:text-xl text-black/90 font-bold'>${item.price * item.quantity}</span>
                         </div>
                         <span className='text-sm text-green-700 mb-1'>{item.availability}</span>
-                        <span className='text-sm text-black/80'>FREE Delivery - In 5 working days</span>
+                        <span className='text-sm text-black/80 mb-3'>FREE Delivery - In 5 working days</span>
                     </div>
                     <div className='flex flex-row gap-4 items-center'>
                         <div className='flex flex-row gap-4 items-center'>
